6376b82b1162281e95616bb5f23e78f9b57c36bb
Austin Dye <thedyeguy1996@outlook.com>
2022-07-31 22:02:10 -0600

    local data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+
+
+export const data = {
+
+ groomers : [{
+ id: 1,
+ name: "Liz",
+ color: "#55555",
+ description:"hi im lizard"
+},
+{
+ id: 2,
+ name: "The Poof",
+ color: "#55555",
+ description:"hi im lizard"
+ },
+{
+ id: 3,
+ name: "Dad",
+ color: "#55555",
+ description:"hi im lizard"
+}
+]
+
+}
+
+
+
+
