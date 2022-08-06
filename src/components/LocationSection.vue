459314ffa6551ea3173fc4f78649114f7c2be808
Austin Dye <thedyeguy1996@outlook.com>
2022-08-06 17:32:58 -0600

    gmaps

--- src/components/LocationSection.vue
+++ src/components/LocationSection.vue
@@ -1,4 +1,22 @@
-<template></template>
+<template>
+  <GMapMap
+    :center="center"
+    :zoom="7"
+    map-type-id="terrain"
+    style="width: 500px; height: 300px"
+  >
+    <GMapCluster>
+      <GMapMarker
+        :key="index"
+        v-for="(m, index) in markers"
+        :position="m.position"
+        :clickable="true"
+        :draggable="true"
+        @click="center = m.position"
+      />
+    </GMapCluster>
+  </GMapMap>
+</template>
 
 <script>
 export default {
