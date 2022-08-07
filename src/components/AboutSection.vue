76b349dc68478f49e38df3a174233f6d8f20aec0
Jackson Hagen <jackson.d.hagen@gmail.com>
2022-08-06 22:06:59 -0600

    about section

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -1,16 +1,23 @@
 <template>
   <div class="row bg-info" @click.stop="goBack()" v-scrollanimation>
-    <div
-      class="col-4 d-flex flex-column justify-content-center align-items-center"
-    >
-      <div class="groom-circle" :style="`--bg-color: ${groomer.color}`">
-        <h3 class="fade-text">{{ groomer.name }}</h3>
-        <img :src="groomer.img" class="groomer-p" />
-        <p class="groomer-text">{{ groomer.name }}</p>
-        <div class="swirly"></div>
-        <div class="profile-button btn">
-          <h4>About</h4>
-          <i class="mdi mdi-chevron-right" @click="showGroomerDetails()"></i>
+    <div class="col-md-3 d-flex align-items-center flex-column text-center">
+      <h2>
+        {{ groomer.name }}
+      </h2>
+      <div class="groom-circle m-0">
+        <img
+          :src="groomer.img"
+          :alt="groomer.name + `'s picture`"
+          class="groomer-p"
+        />
+      </div>
+    </div>
+    <div class="col-9">
+      <div class="row">
+        <div class="col-12">
+          <p>
+            {{ groomer.description }}
+          </p>
         </div>
       </div>
     </div>
@@ -35,4 +42,83 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@import "src/assets/scss/_variables.scss";
+
+.mdi-chevron-right {
+  position: absolute;
+  font-size: 3em;
+  transition: 500ms;
+}
+
+h4 {
+  opacity: 0;
+}
+.swirly {
+  border-radius: 100%;
+  transition: 400ms;
+  position: absolute;
+  width: 18em;
+  height: 18em;
+  background: rgba(208, 67, 255, 0);
+  border-right: 0em rgb(255, 255, 255) solid;
+  border-radius: 100%;
+}
+.profile-button {
+  position: absolute;
+  display: flex;
+  opacity: 1;
+  color: rgba(174, 174, 174, 0.203);
+  bottom: 5.5em;
+  right: 0.2em;
+  transition: 200ms;
+  font-size: 1.75em;
+}
+
+.groom-circle {
+  --bg-color: rgba(0, 0, 255, 0.145);
+  margin-top: 6em;
+  height: 18em;
+  width: 18em;
+  background: var(--bg-color);
+  border-radius: 50%;
+  transition: 400ms;
+  position: relative;
+  display: flex;
+}
+
+.groomer-p {
+  position: absolute;
+  height: 18em;
+  width: 18em;
+
+  border-radius: 100%;
+  transition: 500ms;
+}
+
+.groomer-text {
+  font-size: 1.5em;
+  border-radius: 2%;
+  color: black;
+  position: absolute;
+  left: 2%;
+  top: 10em;
+  bottom: 0.25em;
+  z-index: 100;
+  transition: 300ms;
+  font-family: "Inter", sans-serif;
+  font-weight: bold;
+}
+
+.fade-text {
+  position: absolute;
+  opacity: 0;
+  z-index: 100;
+  top: -2em;
+  width: 8em;
+  height: 1.5em;
+  text-align: center;
+  border-radius: 10%;
+  transition: 400ms;
+}
+</style>
